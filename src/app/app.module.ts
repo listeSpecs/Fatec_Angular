ecb57c6ad0863b9bd0aad4fa12fa79531c52a94a
viclsr <victorialsr0112@gmail.com>
2023-05-23 22:03:02 -0300

    add: componente de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RootComponent } from './root/root.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [
-    CommonModule,BrowserModule, FormsModule
-  ],
-  declarations: [RootComponent],
-  
-  providers:[],
-  bootstrap:[RootComponent]
+  imports: [CommonModule, BrowserModule, FormsModule],
+  declarations: [RootComponent, LoginComponent],
+
+  providers: [],
+  bootstrap: [RootComponent],
 })
-export class AppModule { }
+export class AppModule {}
